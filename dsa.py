7fc7f84a3bf140a394c2f0804cd5adccd85b59d3
ogola-calvin <calvinogola65@gmail.com>
2025-04-14 22:26:17 +0300

    portfolio

---
+++ dsa.py
@@ -0,0 +1,31 @@
+#A binary search  to find a nubmer in sorted dec of cards
+
+def binary_search(cards, target):
+
+    #for position in range(len(cards)):#linear search if list is not sorted
+    #   if cards[position] == target:
+        #    return position
+    #return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cards = [13,12,11,10,9,8,7]
+target = 7
+results = binary_search(cards, target)
+
+print(len(cards))
+print(results)
